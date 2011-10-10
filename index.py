9fe2a42135ad3d810c227747e63e2cdd6e216240
jonathan <jonathan@jonathan-VirtualBox.(none)>
2011-10-10 08:39:58 +0200

    Simons ändringar

--- index.py
+++ index.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 import data
 app = Flask(__name__)
 data.init()
+
 @app.route("/")
 def hello():
-    
-    hej = data.retrieve_techniques()
-    
-    return hej
+    print "Hello"
+    return render_template('main.html')
+
+@app.route("/list")
+def list():
+    print "List"
+    return render_template('list.html', projects = data.retrieve_projects()[1])
+
+@app.route("/techniques")
+def techniques():
+    print "Techniques"
+    return render_template('main.html')
+
+@app.route("/project/<int:id>")
+def show_project():
+    return render_template('main.html')
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
